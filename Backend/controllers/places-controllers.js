a97ad047bf46a6e2d389e643fa5771cf99c5092a
Jeremy Marino <jermarino16@gmail.com>
2020-06-25 06:37:30 -0700

    added correct authorization for editing and deleting for correct users

--- Backend/controllers/places-controllers.js
+++ Backend/controllers/places-controllers.js
@@ -146,6 +146,11 @@ const updatePlace = async (req, res, next) => {
     return next(error);
   }
 
+  if (place.creator.toString() !== req.userData.userId) {
+    const error = new HttpError("You are not allowed to edit this place.", 401);
+    return next(error);
+  }
+
   place.title = title;
   place.description = description;
 
@@ -172,6 +177,13 @@ const deletePlace = async (req, res, next) => {
     const error = new HttpError(
       "Something went wrong, could not delete place.",
       500
+    );
+    return next(error);
+  }
+  if (place.creator.id !== req.userData.userId) {
+    const error = new HttpError(
+      "You are not allowed to delete this place.",
+      401
     );
     return next(error);
   }
