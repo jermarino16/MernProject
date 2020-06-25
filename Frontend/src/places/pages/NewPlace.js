0a3d3da716b0500e75d3f12c2462b06ba1361f63
Jeremy Marino <jermarino16@gmail.com>
2020-06-25 06:19:34 -0700

    wired up new place creation to use jwt

--- Frontend/src/places/pages/NewPlace.js
+++ Frontend/src/places/pages/NewPlace.js
@@ -51,12 +51,9 @@ const NewPlace = () => {
       formData.append("address", formState.inputs.address.value);
       formData.append("creator", auth.userId);
       formData.append("image", formState.inputs.image.value);
-      const responseData = await sendRequest(
-        "http://localhost:5000/api/places",
-        "POST",
-        formData
-      );
-      console.log(responseData);
+      await sendRequest("http://localhost:5000/api/places", "POST", formData, {
+        Authorization: "Bearer " + auth.token,
+      });
       history.push("/");
     } catch (err) {}
   };
@@ -92,7 +89,6 @@ const NewPlace = () => {
           onInput={inputHandler}
         />
         <ImageUpload
-          center
           id="image"
           onInput={inputHandler}
           errorText="Please provide an image."
