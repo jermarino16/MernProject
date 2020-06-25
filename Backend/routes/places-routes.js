d97b9cd22d3c6c935e8fc4d499dfafdba9491e6c
Jeremy Marino <jermarino16@gmail.com>
2020-06-25 06:05:51 -0700

    added back dependency in file

--- Backend/routes/places-routes.js
+++ Backend/routes/places-routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check } = require("express-validator");
 
 const placesControllers = require("../controllers/places-controllers");
 const fileUpload = require("../middleware/file-upload");
